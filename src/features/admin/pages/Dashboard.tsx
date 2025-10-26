18016fe7aaecfc04d9e664ee083b310057b11fd6
Abdelfattah <your.email@example.com>
2025-10-26 15:20:47 +0300

    feact: add logout button in dashboard page for testing

--- src/features/admin/pages/Dashboard.tsx
+++ src/features/admin/pages/Dashboard.tsx
@@ -1,6 +1,16 @@
+import { AuthContext } from "@/contexts/AuthContext";
+import { useContext } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Dashboard() {
+  const authContext = useContext(AuthContext);
+  if (!authContext) {
+    throw new Error("AuthContext not found");
+  }
+  const { logout } = authContext;
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <Button onClick={() => logout()}>Logout</Button>
+    </div>
+  );
 }
